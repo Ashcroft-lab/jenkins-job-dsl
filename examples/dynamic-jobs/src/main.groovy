8efb2f2b866bfa82b086c581e45cbcc6d991453b
Ashcroft-lab <iadarshgupta@gmail.com>
2024-08-30 23:19:18 +0530

    mior update

--- examples/dynamic-jobs/src/main.groovy
+++ examples/dynamic-jobs/src/main.groovy
@@ -37,7 +37,50 @@ for (pipeline in pipeline_file_list) {
     pipelineJob(job_config.get_job_name()) {
     definition {
         cps {
-            script(readFileFromWorkspace('examples/dynamic-jobs/src/pipelie_script.txt'))
+            script("""
+pipeline {
+    agent any
+
+    stages {
+
+        stage('Checkout') {
+            steps {
+                echo 'Checking out source code...'
+            }
+        }
+
+        stage('Build') {
+            steps {
+                echo '${job_config.get_build_command()}'
+            }
+        }
+
+        stage('Test') {
+            steps {
+                echo 'Running tests...'
+            }
+        }
+
+        stage('Deploy DEV') {
+            steps {
+                echo 'Deploying the project...'
+            }
+        }
+
+        stage('Deploy QA') {
+            steps {
+                echo 'Deploying the project...'
+            }
+        }
+
+        stage('Deploy PROD') {
+            steps {
+                echo 'Deploying the project...'
+            }
+        }
+    }
+}
+""")
             sandbox()
         }
     }
